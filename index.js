a7852ef3dbcf5cc6b47e8a0ef6b54a3d79232146
Surabhi <surabhi.sharma22@gmail.com>
2019-05-13 10:51:47 +0200

    Added comment

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import ListWithEditDelete from './ListWithEditDelete';
 import FormUI from './FormUI';
 import {name as appName} from './app.json';
-
+// Add a comment
 AppRegistry.registerComponent(appName, () => ListWithEditDelete);
